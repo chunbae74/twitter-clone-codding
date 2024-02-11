d7987fdb5dc898353151019979109aa62703f498
코딩하는 춘배 <kaan03@naver.com>
2024-02-11 22:28:26 +0900

    24.02.11 학습시작

---
+++ src/firebase.tsx
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCBXJTP6KiAlJu8ryPa7IrHY9eh-sFnc14",
+  authDomain: "twitter-clone-coding-28642.firebaseapp.com",
+  projectId: "twitter-clone-coding-28642",
+  storageBucket: "twitter-clone-coding-28642.appspot.com",
+  messagingSenderId: "1020667855627",
+  appId: "1:1020667855627:web:2309e1b09be502dc4a580e",
+};
+
+const app = initializeApp(firebaseConfig);
